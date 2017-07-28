e866384f49059f53c763e56df9270c75eeb56c62
Bruno Agutoli <bruno.agutoli@gmail.com>
2017-07-28 15:47:56 -0300

    added a demo

---
+++ demo/demo.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Select from '../lib'
+
+const options = [
+  { value: 'one', label: 'One' },
+  { value: 'two', label: 'Two' },
+  { value: 'tree', label: 'Tree' },
+]
+
+ReactDOM.render((
+  <div>
+    <div style={{width: '300px'}}>
+      <h2>default</h2>
+      <Select
+        name="form-field-name"
+        options={options}
+      />
+    </div>
+    <div style={{width: '300px'}} className="select-theme-1">
+      <h2>theme 1</h2>
+      <Select
+        name="form-field-name"
+        options={options}
+      />
+    </div>
+    <div style={{width: '300px'}} className="select-theme-2">
+      <h2>theme 2</h2>
+      <Select
+        name="theme"
+        options={options}
+      />
+    </div>
+    <h2>CSS example</h2>
+    <pre style={{color: 'grey', border: '2px solid #ccc', padding: '5px'}}>
+    {`
+    :root {
+      // change CSS root scope
+      --styled-select-border-style: solid;
+    }
+
+    .select-theme-1 {
+      --styled-select-background-color: black;
+      --styled-select-border-width: 3px;
+      --styled-select-control-border-color: red;
+    }
+
+    .select-theme-2 {
+      --styled-select-background-color: red;
+      --styled-select-border-width: 1px;
+      --styled-select-control-border-color: black;
+    }`}
+    </pre>
+  </div>
+), document.getElementById('main'));
---
+++ demo/webpack.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  entry: './demo/demo.js',
+  output: {
+    filename: './demo/demo.bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  }
+}
