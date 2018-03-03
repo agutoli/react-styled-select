482481dc27dbea684ead8e9e93756cd6bb34c71e
Bruno Agutoli <bruno.agutoli@gmail.com>
2018-03-02 23:01:32 -0300

    update readme

--- bin/extract-css-variables.js
+++ bin/extract-css-variables.js
@@ -3,8 +3,8 @@ const variables = require('../src/Select/cssVariables');
 
 Object.keys(variables).forEach((varname) => {
   const splitted = varname.split('__')
-  let suffix = `<a href="https://css-tricks.com/almanac/properties/f/${splitted[1]}>${splitted[1]}</a>`
-  let splited = suffix.split('--')
+  let suffix = `<a href="https://css-tricks.com/almanac/properties/f/${splitted[1]}">${splitted[1]}</a>`
+  let splited = splitted[1].split('--')
   if (splited.length > 1) {
     suffix = `<a href="https://css-tricks.com/almanac/properties/f/${splited[0]}">${splited[0]}</a>--<b>${splited[1]}</b>`
   }
